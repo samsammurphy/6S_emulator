9c1e462545af3ec0ed919830d27d858750e83da7
Sam Murphy <samsammurphy@gmail.com>
2016-11-21 16:38:24 -1000

    added SR and PD options for validation plot (i.e. the intervals expressed as surface reflectance or percentage difference)
    Former-commit-id: f49d66d5cfa9397909b97ad03bc1a35159df8398 [formerly 70838561031f6b7b18b5861b2507891cd7a28f6f]
    Former-commit-id: 1e918995dedc69fae690a3c9acdabff7f1bc1a7c

--- z/validation/LUT_validation_plot.py
+++ z/validation/LUT_validation_plot.py
@@ -18,11 +18,28 @@ from matplotlib import pylab as plt
 
 
 
-def read_confidence(fname,confidence_level):
-  stats = pickle.load(open(fname,'rb'))
-  return stats['confidence'][confidence_level]
+def read_confidence(fname,confidence_level,sr=False):
+  """
+  Reads confidence interval for given filename and confidence level (e.g. 95%)
   
-def read_stats(refs,stats_dir,confidence_level):
+  Optionally convert from '% difference' to 'surface refletance' interval
+  
+  """
+  
+  # percentage difference statistics
+  pd = pickle.load(open(fname,'rb'))
+  
+  # pd interval
+  interval = pd['confidence'][confidence_level]
+
+  # surface reflectance interval?
+  if sr:
+    interval = sr * interval/100
+  
+  return interval
+  
+  
+def read_stats(refs,stats_dir,confidence_level,sr_interval=False):
     
   # confidence arrays
   b = []# blue
@@ -32,6 +49,7 @@ def read_stats(refs,stats_dir,confidence_level):
   s1 =[]# swir1
   s2 =[]# swir2
 
+  # fill confidence arrays for each surface reflectance value
   for ref in refs:
     refdir = '{}/{:.2f}'.format(stats_dir,ref)
     try:
@@ -43,19 +61,23 @@ def read_stats(refs,stats_dir,confidence_level):
     fnames = glob.glob('*.stats')
     fnames.sort()
     # append results to confidence arrays
-    b.append(read_confidence(fnames[0],confidence_level))
-    g.append(read_confidence(fnames[1],confidence_level))
-    r.append(read_confidence(fnames[2],confidence_level))
-    n.append(read_confidence(fnames[3],confidence_level))
-    s1.append(read_confidence(fnames[4],confidence_level))
-    s2.append(read_confidence(fnames[5],confidence_level))
-
+    if sr_interval:
+      sr=ref
+    else:
+      sr=False
+    b.append(read_confidence(fnames[0],confidence_level,sr=sr))
+    g.append(read_confidence(fnames[1],confidence_level,sr=sr))
+    r.append(read_confidence(fnames[2],confidence_level,sr=sr))
+    n.append(read_confidence(fnames[3],confidence_level,sr=sr))
+    s1.append(read_confidence(fnames[4],confidence_level,sr=sr))
+    s2.append(read_confidence(fnames[5],confidence_level,sr=sr))
+  
   return {'b':b,'g':g,'r':r,'n':n,'s1':s1,'s2':s2}      
       
 
-def plot_stats(refs,stats_dir,confidence_level):
-  
-  stats = read_stats(refs,stats_dir,confidence_level)
+def plot_stats(refs,stats_dir,confidence_level,sr_interval=False):
+    
+  stats = read_stats(refs,stats_dir,confidence_level,sr_interval=sr_interval)
   
   plt.plot(refs,stats['b'],'b',linewidth=2)
   plt.plot(refs,stats['g'],'g',linewidth=2)
@@ -64,8 +86,12 @@ def plot_stats(refs,stats_dir,confidence_level):
   plt.plot(refs,stats['s1'],'y',linewidth=2)
   plt.plot(refs,stats['s2'],'m',linewidth=2)
   
-  plt.ylim(0,50)
-  plt.ylabel('{}% confidence interval'.format(confidence_level))
+  plt.title('{}% confidence level'.format(confidence_level))
+  if sr_interval:
+    plt.ylabel('reflectance interval')
+  else:
+    plt.ylim(0,50)
+    plt.ylabel('percentage interval (%)')
   plt.xlabel('surface reflectance')
   
   
@@ -85,8 +111,8 @@ def main():
   validation_dir = os.path.dirname(os.path.abspath(__file__))
   stats_dir = '{}/stats/{}/viewz_0/'.format(validation_dir,config)
     
-  # plot stats
-  plot_stats(refs, stats_dir,confidence_level)
+  # plot stats: either i) percent difference (default) or ii) SR difference
+  plot_stats(refs, stats_dir,confidence_level,sr_interval = False)
   
 if __name__ == '__main__':
   main()

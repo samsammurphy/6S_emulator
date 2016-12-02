48d84ddf6295d1ebf1b4fa1b6f992f710d02214a
Sam Murphy <samsammurphy@gmail.com>
2016-12-02 12:47:36 -1000

    supports multiple channels
    Former-commit-id: 5b5737a318455856b113dbd497628785173074aa [formerly 66a803cf92c0840e1cbd4cae634b4ee909ccb9c6]
    Former-commit-id: c0bddf740c837dd7ad74e12b747c361d6a07310d

--- z/validation/MonteCarlo/MonteCarlo_Histoplot.py
+++ z/validation/MonteCarlo/MonteCarlo_Histoplot.py
@@ -15,6 +15,9 @@ from matplotlib import pylab as plt
 
 
 def load_performance_stats(channel):
+  """
+  Loads all available MonteCarlo stats for a given channel of Landsat 8
+  """
   file_dir = os.path.dirname(os.path.abspath(__file__)) 
   stat_dir = os.path.join(file_dir,'stats',channel)
   try:
@@ -29,6 +32,7 @@ def load_performance_stats(channel):
     print('statistics loading error for: '+stat_dir)
     sys.exit(1)  
   
+    
 def dref_slice(drefs, refs, ref):
   """
   Performance metric is delta reflectance (dref). It was calculated for a 
@@ -40,7 +44,19 @@ def dref_slice(drefs, refs, ref):
     sys.exit()
   return np.array([ds[index][0] for ds in drefs])
 
-def plot_histograms(dref,ref,channel):
+def color_from_channel(channel):
+  switch = {
+            'blue':'blue',
+            'green':'green',
+            'red':'red',
+            'nir':'yellow',
+            'swir1':'cyan',
+            'swir2':'magenta'
+            }
+  return switch[channel]
+  
+
+def plot_histograms(ref,channels):
   """
   Plots two histograms 1) percentage difference, 2) delta reflectance
   """
@@ -48,38 +64,45 @@ def plot_histograms(dref,ref,channel):
   ax1 = fig.add_subplot(211)
   ax2 = fig.add_subplot(212)
   
-  # percentage difference histograms
-  bins = np.linspace(-10,10,101)
-  pd = 100*dref/ref
-  ax1.hist(pd, bins, normed=1, facecolor=channel)
-  ax1.set_title('percentage difference')
+  for channel in channels:
+    
+    # load performance statistics (all SRs)
+    drefs, refs = load_performance_stats(channel)
+      
+    # stats for this reflectance
+    dref = dref_slice(drefs,refs,ref)
   
-  # dref histogram
-  bins = ref*bins/100 # convert to reflectance values (but in same % range)
-  ax2.hist(dref, bins ,normed=1, facecolor=channel)
-  ax2.set_xlim((min(bins),max(bins)))
-  ax2.set_title('delta reflectance')
+    # color of channel
+    color = color_from_channel(channel)
+    
+    # percentage difference histograms
+    bins = np.linspace(-10,10,101)
+    pd = 100*dref/ref
+    ax1.hist(pd, bins, normed=1, facecolor=color)
+    ax1.set_title('percentage difference')
+    
+    # dref histogram
+    bins = ref*bins/100 # convert to reflectance values (but in same % range)
+    ax2.hist(dref, bins ,normed=1, facecolor=color)
+    ax2.set_xlim((min(bins),max(bins)))
+    ax2.set_title('delta reflectance')
+    
+    print('{} 95% confidence = {}'.format(channel,np.percentile(abs(pd),95)))
   
   fig.tight_layout()
-  
-  print(np.percentile(abs(pd),95))
 
+  
 def main():
   
-  # channel name
-  channel = 'blue'
-  
   # surface reflectance (SR)
-  ref = 0.1
-  
-  # load performance statistics (all SRs)
-  drefs, refs = load_performance_stats(channel)
-    
-  # stats for this SR
-  dref = dref_slice(drefs,refs,ref)
+  ref = 0.05
+
+  # channel name
+  channels = ['blue','green','red','nir','swir1','swir2']
 
   # histogram plot
-  plot_histograms(dref,ref,channel)
+  plot_histograms(ref,channels)
+  
   
 if __name__ == '__main__':
   main()
